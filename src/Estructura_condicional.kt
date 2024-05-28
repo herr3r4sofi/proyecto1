1440b000f4fedfdd37e02cf5aa15ac2b2de5623b
herr3r4sofi <shc290108@gmail.com>
2024-05-28 10:52:57 -0600

    Condicional expresion

--- src/Estructura_condicional.kt
+++ src/Estructura_condicional.kt
@@ -1,47 +1,46 @@
-fun main() {
-    /*
-    //problema 1
-    print("Ingrese el sueldo del empleado:")
-    val sueldo: Double = readln().toDouble()
+fun main(){}
+/*//problema 1
+print("Ingrese el sueldo del empleado:")
+val sueldo: Double = readln().toDouble()
 
-    // condicional if, si el sueldo es mayor 3000,
-    // imprimir "Debe pagar impuestos"
-    if(sueldo > 3000)
-        println("Debe pagar impuesto")
+// condicional if, si el sueldo es mayor 3000,
+// imprimir "Debe pagar impuestos"
+if(sueldo > 3000)
+    println("Debe pagar impuesto")
 }
 */
-    /*  //problema 2
-      print("Ingrese primer valor:")
-      val valor1 = readln().toInt()
-      print("Ingrese egundo valor:")
-      val valor2 = readln().toInt()
-      //condicional con if / else
-      if (valor1 > valor2) {
-          print("El valor es $valor1")
-      } else {
-          print("El valor es $valor2")
-      }
+/*  //problema 2
+  print("Ingrese primer valor:")
+  val valor1 = readln().toInt()
+  print("Ingrese egundo valor:")
+  val valor2 = readln().toInt()
+  //condicional con if / else
+  if (valor1 > valor2) {
+      print("El valor es $valor1")
+  } else {
+      print("El valor es $valor2")
   }
-  */
+}
+*/
 //problema 3
-    /*print("Ingrese el primer valor:")
-    val valor1: Int = readln().toInt()
-    print("Ingrese el segundo valor:")
-    val valor2: Int = readln().toInt()
-    if (valor1 < valor2){
-    val suma: Int = valor1 + valor2
-    val resta: Int = valor1 - valor2
-        println("La suma de los dos valores es: $suma")
-        println("La resta de los dos valores es: $resta")
-    }else{
-        val producto: Int = valor1 * valor2
-        val division: Int = valor1 / valor2
-        println("El producto de los dos valores es: $producto")
-        println("La divison de los dos valores es: $division")
-    }
+/*print("Ingrese el primer valor:")
+val valor1: Int = readln().toInt()
+print("Ingrese el segundo valor:")
+val valor2: Int = readln().toInt()
+if (valor1 < valor2){
+val suma: Int = valor1 + valor2
+val resta: Int = valor1 - valor2
+    println("La suma de los dos valores es: $suma")
+    println("La resta de los dos valores es: $resta")
+}else{
+    val producto: Int = valor1 * valor2
+    val division: Int = valor1 / valor2
+    println("El producto de los dos valores es: $producto")
+    println("La divison de los dos valores es: $division")
+}
 }
 
-     */
+ */
 
 // problema 4
 /*println("Ingrese una primera nota:")
@@ -59,15 +58,17 @@ fun main() {
     }    }
  */
 
-    //problema 5
-    print("Ingrese un numero del 1 al 99:")
-    val numero: Int = readln().toInt()
-    if (numero >= 10){
-        println("El numero contiene dos digitos")
-    }else{
-        println("El numero contiene un digito")
-    }
-}
+/* //problema 5
+ print("Ingrese un numero del 1 al 99:")
+ val numero: Int = readln().toInt()
+ if (numero >= 10){
+     println("El numero contiene dos digitos")
+ }else{
+     println("El numero contiene un digito")
+ }
+}*/
+
+
     
 
 
---
+++ src/condicional_expresion.kt
@@ -0,0 +1,33 @@
+fun main() {
+    /*//problema 1
+    print("Ingrese un primer valor:")
+    val valor1 = readln().toInt()
+    print("Ingrese un segundo valor:")
+    val valor2 = readln().toInt()
+    val mayor = if (valor1 > valor2) valor1 else valor2
+    println("El mayor entre $valor1 y $valor2 es $mayor")
+    }*/
+//problema 2
+    /* print("Ingrese un valor entero:")
+     val valor = readln().toInt()
+     val resultado = if (valor % 2 == 0) {
+         print("Cuadrado:")
+         valor * valor
+     }else{
+    println("Cubo:")
+         valor * valor * valor
+     }
+     println(resultado)
+    }*/
+
+    //problema propuesto
+    /*print("Ingresee un numero entre el 1 y 99:")
+    val numero = readln().toInt()
+    val cantidad = if (numero >= 10) {
+        println("El numero tiene dos digitos")
+    } else {
+        println("El numero tiene un digito")
+    }
+} */
+
+
